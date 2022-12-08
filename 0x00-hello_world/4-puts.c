ecb728b2f1d66d97c284e07c28e1e12db946053e
Jomare22 <abelsjomare@gmail.com>
2022-12-08 11:51:39 -0800

    Altered coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Return: ALways 0 (Success)
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle\n");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
