3ee5d56b8517e0d2113d6b82bc828f52e5fee870
Jomare22 <abelsjomare@gmail.com>
2022-12-09 08:13:25 -0800

    Updated task 4 and first attempt at task 5

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase,
- * followed by a new line, except q and e
+ * main - Function
+ *
  * Return: Aways 0 (Sucess)
  */
 int main(void)
