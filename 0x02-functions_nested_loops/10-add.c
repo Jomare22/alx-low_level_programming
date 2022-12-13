00d33d45bc5e35d583f53bef824e12644a5f4d81
Jomare22 <abelsjomare@gmail.com>
2022-12-13 09:23:14 -0800

    First commit task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - addition calculation of two integers
+ * @a: int to be used in addition
+ * @b: int to be used in addition
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+int c;
+
+c = a + b;
+return (c);
+}
