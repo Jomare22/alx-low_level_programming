94093f10faaff07020fa6c1af47776470290f809
Jomare22 <abelsjomare@gmail.com>
2022-12-14 00:10:31 -0800

    First commit new directory

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check if number is positive or negative
+ *
+ * Return: ALways 0 (Success)
+ *
+ */
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+return (0);
+}
