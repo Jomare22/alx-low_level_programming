1ea7577de9ea35c87059054ff6ef08720a45d72f
Jomaré <105128755+Jomare22@users.noreply.github.com>
2022-12-14 10:49:42 +0200

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 int i;
- 
+
 printf("Infinite loop incoming :(\n");
-  
+
 i = 0;
-  
+
 /**
  * while (i < 10)
  *{
@@ -20,6 +20,6 @@ i = 0;
  *}
  */
 printf("Infinite loop avoided! \\o/\n");
-  
+
 return (0);
 }
