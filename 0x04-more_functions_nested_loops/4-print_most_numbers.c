58e985f6e7d8b787c52ae7a1cd95c046a45e6423
Jomare22 <abelsjomare@gmail.com>
2022-12-15 00:10:27 -0800

    First commit taks 3 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints number 0-9, excluding 2 and 4.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+int c;
+for (c = 0; c < 10; c++)
+{
+if (c != 2)
+{
+if (c != 4)
+{
+_putchar(c);
+}
+}
+}
+_putchar('\n');
+}
