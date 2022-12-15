04223e6f9464c3a94888e483c9dda9090086be9a
Jomare22 <abelsjomare@gmail.com>
2022-12-15 00:37:16 -0800

    Fist commit task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal line.
+ * @n: int for length of line.
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i, j;
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+if (j == i)
+{
+_putchar('\\');
+}	      
+else if (j < i)
+{		
+_putchar(' ');
+}	      
+}
+_putchar('\n');
+}
+}
+}
