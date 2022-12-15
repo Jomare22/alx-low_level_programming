ed6fdd7c4ba3057735e94a6c4353bb13812f71c6
Jomare22 <abelsjomare@gmail.com>
2022-12-14 23:51:21 -0800

    First commit task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: int variable to be checked
+ * Return: 1 (If digit) 0 (Other)
+ */
+int _isdigit(int c)
+{
+return (c >= '0' && c <= '9');
+}
