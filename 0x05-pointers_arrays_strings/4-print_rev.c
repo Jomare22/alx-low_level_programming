836e73e09d8bb148755290432ed9722bb0e74e24
Jomaré <105128755+Jomare22@users.noreply.github.com>
2022-12-21 15:26:39 +0200

    Update task 4 naming conv and readability

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,50 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-
- * print_rev - imprime en reversa
-
- * @s: string
-
+ * print_rev - prints a string in reverse
+ * @s: string to reverse
  * return: 0
-
  */
-
 void print_rev(char *s)
-  
 {
-  
-  int longi = 0;
-  
-  int o;
-  
+int length = 0;
+int o;
 
-  
-  while (*s != '\0')
-    
-    {
-      
-      longi++;
-      
-      s++;
-      
-    }
-  
-  s--;
-  
-  for (o = longi; o > 0; o--)
-    
-    {
-      
-      _putchar(*s);
-      
-      s--;
-      
-    }
-  
-
-  
-  _putchar('\n');
-  
+while (*s != '\0')
+{
+length++;
+s++;
 }
-
+s--;
+for (o = length; o > 0; o--)
+{
+_putchar(*s);
+s--;
+}
+_putchar('\n');
+}
