df0a4678e5f03190783ff96f5022b94546ca1324
Jomare22 <abelsjomare@gmail.com>
2022-12-21 04:58:34 -0800

    Adding a new directory and task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a pointer receiving an int and updates the value to 98.
+ * @n: int as parameter
+ * Return: n
+ */
+void reset_to_98(int *n)  
+{
+*n = 98; 
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+# Exploring pointers, arrays and strings.
